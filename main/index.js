4141e74616e7b0ac2782e3edb9ea114e9ea732c7
jesse h <jesselevihall@gmail.com>
2022-01-12 14:55:43 -0700

    Data Sync Basic

---
+++ main/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      text: 'initial text'
+    }
+  }
+
+  handleChange(event){
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.text} <br/>
+        <input type="text" value={this.state.text} onChange={this.handleChange.bind(this)}/>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
